f8328c48987ceb181fa148c6ff4e6f6836c2b1a6
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-10-17 08:15:39 +0100

    added image (needle) to main section

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function Main() {
     return <div className="main-section">
         <h1 className="main-t">MOMO CREATES</h1>
+        <img className="needle-logo" src={require('./images/needle.png')} alt="needle"></img>
         <p className="main-p">Your vision, woven into reality...</p>
     </div>
 }
