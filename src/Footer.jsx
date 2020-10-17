948824ea93b672d6dbbc44c9208b170b41bda6d8
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-10-17 07:44:02 +0100

    amended CSS (properties ignored)

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     const currentYear = new Date().getFullYear();
     return <footer>
-    <a target="_blank" href="https://www.instagram.com/"><img className="insta-img" src={require('./images/instagram-logo.png')} alt="instagram"></img></a>
+    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/"><img className="insta-img" src={require('./images/instagram-logo.png')} alt="instagram"></img></a>
     <p className="copyright">© Copyright {currentYear}. MOMO CREATES. All Rights Reserved.</p>
 
     </footer>
