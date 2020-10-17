9c5bdecafd760973478d6ec907f8fb2be12c2ab2
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-10-17 08:10:13 +0100

    added main section (name and slogan)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Main from "./Main";
 
 function App() {
     return <div>
     <Header />
+    <Main />
     <Footer />
     </div>
 }
---
+++ src/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Main() {
+    return <div className="main-section">
+        <h1 className="main-t">MOMO CREATES</h1>
+        <p className="main-p">Your vision, woven into reality...</p>
+    </div>
+}
+
+export default Main;
