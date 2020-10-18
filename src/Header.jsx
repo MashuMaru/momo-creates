a9dc66d51db6120d1f1dbb8a272cfba7bd4151b3
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-10-18 10:02:37 +0100

    added bootstrap to header (toggle button, navbar)

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 
 function Header() {
-    return <div className="header">
-            <p className="header-momo">MOMO CREATES</p>
-                <div className="header-links">
-                    <a className="links" href="www.google.com">Home</a>
-                    <a className="links" href="www.google.com">Why Choose Momo</a>
-                    <a className="links" href="www.google.com">Recent Works</a>
-                    <a className="links" href="www.google.com">Contact</a>
+    return <nav className="navbar navbar-expand-lg navbar-light header">
+            <a className="header-momo" href="https://getbootstrap.com/docs/4.5/components/navbar/">MOMO CREATES</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse ml-auto" id="navbarNavAltMarkup">
+                    <div className="navbar-nav ml-auto">
+                    <a className="links nav-link header-links ml-auto" href="https://getbootstrap.com/docs/4.5/components/navbar/">Home</a>
+
+                    <a className="links nav-link header-links ml-auto" href="https://getbootstrap.com/docs/4.5/components/navbar/">Why Choose Momo Creates</a>
+
+                    <a className="links nav-link header-links ml-auto" href="https://getbootstrap.com/docs/4.5/components/navbar/">Recent Works</a>
+
+                    <a className="links nav-link header-links ml-auto" href="https://getbootstrap.com/docs/4.5/components/navbar/">Contact</a>
+                    </div>
                 </div>
-            </div>
+            </nav>
 }
 
 export default Header;
