143794b5a4b1f22e807c929eb7c276e6cddfb7c0
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-10-19 09:49:37 +0100

    amended carousel buttons (didn't function)

--- src/Recent.jsx
+++ src/Recent.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Recent() {
-    return <section id="recent" className="recent-section carousel slide" data-ride="carousel">
+    return <section id="carouselExampleIndicators" className="recent-section carousel slide" data-ride="carousel">
     <h1 className="recent-heading">RECENT WORK</h1>
     <ol className="carousel-indicators">
-      <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active active-indicator"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active active-indicator"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
       {/* <li data-target="#carouselExampleCaptions" data-slide-to="2"></li> */}
     </ol>
     <div className="carousel-inner">
@@ -31,11 +31,11 @@ function Recent() {
         </div>
       </div> */}
     </div>
-    <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
       <span className="carousel-control-prev-icon" aria-hidden="true"></span>
       <span className="sr-only">Previous</span>
     </a>
-    <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
       <span className="carousel-control-next-icon" aria-hidden="true"></span>
       <span className="sr-only">Next</span>
     </a>
