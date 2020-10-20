fdbad76ef099daf12283623cc45a0cd1911b3c56
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-10-20 06:53:22 +0100

    added JS to instagram icon (open page + in new tab)

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+function instagram() {
+    window.open('https://www.instagram.com/', '_blank');
+}
+
 function Footer() {
     const currentYear = new Date().getFullYear();
     return <footer>
-    <a class="fab fa-instagram" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/"></a>
+    <i class="fab fa-instagram inst-message" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/" onClick={instagram}></i>
     {/* <img className="insta-img" src={require('./images/instagram-logo.png')} alt="instagram"></img> */}
     <p className="copyright">© Copyright {currentYear}. Momo Creates. All Rights Reserved.</p>
 
