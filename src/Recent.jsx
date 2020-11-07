5c08e5ee42b1d61bfa9dce02ffdfc0c940d2bd23
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-11-07 08:36:54 +0000

    deleted irrelevent code

--- src/Recent.jsx
+++ src/Recent.jsx
@@ -1,39 +1,27 @@
 import React from "react";
 
-function enlarge() {
-    alert('Enlarge this image.');
-}
-
 function Recent() {
     return <section id="carouselExampleIndicators" className="recent-section carousel slide" data-ride="carousel">
     <h1 className="recent-heading">RECENT WORK</h1>
     <ol className="carousel-indicators">
       <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active active-indicator"></li>
       <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      {/* <li data-target="#carouselExampleCaptions" data-slide-to="2"></li> */}
     </ol>
     <div className="carousel-inner">
       <div className="carousel-item active img-fluid">
-        <img src={require('./images/recent-1.jpg')} className="recent-img d-block" alt="..." onClick={enlarge}></img>
+        <img src={require('./images/recent-1.jpg')} className="recent-img d-block" alt="..."></img>
         <div className="carousel-caption">
           <h5 className="recent-title">Rassams Creamery</h5>
           <p className="recent-p">Main logo embroidery to restaurant furniture. </p>
         </div>
       </div>
       <div className="carousel-item img-fluid">
-        <img src={require('./images/recent-2.JPG')} className="recent-img d-block" alt="img-emb" onClick={enlarge}></img> 
+        <img src={require('./images/recent-2.JPG')} className="recent-img d-block" alt="img-emb"></img> 
         <div className="carousel-caption">
           <h5 className="recent-title">Holy Goat Customs</h5>
           <p className="recent-p">Logo designs embroidery to motorcycle seats.</p>
         </div>
       </div>
-      {/* <div className="carousel-item container-fluid">
-        <img src={require('./images/mainsection-img.png')} className="recent-img d-block w-100" alt="..."></img>
-        <div className="carousel-caption d-none d-md-block">
-          <h5>Embroidery work 3</h5>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </div>
-      </div> */}
     </div>
     <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
       <span className="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -49,7 +37,3 @@ function Recent() {
 }
 
 export default Recent;
-
-// img-fluid was container-fluid
-
-// "d-none d-md-block" in div before the h5 and p caption for images. 
