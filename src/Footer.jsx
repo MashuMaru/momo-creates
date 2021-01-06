ec5757d5246f27e9e84992589cb9bd19b8c26739
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-01-06 20:02:47 +0000

    minor CSS amendment. Start of REVAMP of site.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -12,6 +12,7 @@ function Footer() {
     <i className="fab fa-instagram inst-message" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/" onClick={instagram}></i>
     {/* <img className="insta-img" src={require('./images/instagram-logo.png')} alt="instagram"></img> */}
     <p className="copyright">© Copyright {currentYear}. Momo Creates. All Rights Reserved.</p>
+    <p className="copyright">Website designed and developed by <a href="https://mashumaru-dev.netlify.app/" rel="noreferrer">Mashumaru.</a></p>
 
     </footer>
 }
